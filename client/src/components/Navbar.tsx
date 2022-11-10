96ef8f927719cbe260caf530e34c14bce5877cb6
Nik Pietanze <nikpietanze@gmail.com>
2022-11-09 19:23:15 -0500

    create sign in button; move avatar to right side of nav;

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -51,13 +51,13 @@ const Navbar: Component = () => {
     createEffect(() => toggleTheme(theme()));
 
     return (
-        <nav class="navbar bg-base-100 border-b border-slate-700">
+        <nav class={`navbar bg-base-100 border-b ${theme() === Themes.dark ? "border-slate-700" : "border-slate-300"} px-6`}>
             <div class="navbar-start">
                 <div class="dropdown">
                     <label tabindex="0" class="btn btn-ghost btn-circle">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
-                    <ul tabindex="0" class="menu menu-normal dropdown-content mt-3 p-2 shadow-lg bg-base-100 rounded-lg w-52">
+                    <ul tabindex="0" class="menu menu-normal dropdown-content mt-3 p-2 shadow-md bg-base-100 rounded-lg w-52">
                         <li><a href="/">Home</a></li>
                     </ul>
                 </div>
@@ -73,29 +73,19 @@ const Navbar: Component = () => {
                     <input type="checkbox" class="toggle toggle-md bg-slate-600" checked={savedTheme && savedTheme === Themes.light} onClick={(e) => setTheme(e.target.checked ? Themes.light : Themes.dark)} />
                     <svg class="h-6" viewBox="0 0 24 24" style="fill: none; stroke: currentcolor;"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
                 </div>
-                <button class="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                </button>
-                <div class="dropdown dropdown-end">
-                    <label tabindex="0" class="btn btn-ghost btn-circle flex justify-center items-center">
-                        <Show
-                            when={!loading() && user()}
-                            fallback={<img class="rounded-full shadow shadow-stone-900 h-10 w-10" src="" />}
-                        >
+                <Show
+                    when={!loading() && user()}
+                    fallback={<button class="btn btn-ghost" onClick={() => loginWithRedirect()}>Sign in</button>}
+                >
+                    <div class="dropdown dropdown-end">
+                        <label tabindex="0" class="btn btn-ghost btn-circle flex justify-center items-center">
                             <img class="rounded-full shadow shadow-stone-900 h-10 w-10" src={user().picture} />
-                        </Show>
-                    </label>
-                    <ul tabindex="0" class="menu menu-normal dropdown-content mt-3 p-2 shadow-lg bg-base-100 rounded-lg w-52">
-                        <Show
-                            when={!loading() && !isAuthenticated()}
-                            fallback={
-                                <li><span onClick={() => logout()}>Logout</span></li>
-                            }
-                        >
-                            <li><span onClick={() => loginWithRedirect()}>Login</span></li>
-                        </Show>
-                    </ul>
-                </div>
+                        </label>
+                        <ul tabindex="0" class="menu menu-normal dropdown-content mt-3 p-2 shadow-md bg-base-100 rounded-lg w-52">
+                            <li><span onClick={() => logout()}>Sign out</span></li>
+                        </ul>
+                    </div>
+                </Show>
             </div>
         </nav >
     );
